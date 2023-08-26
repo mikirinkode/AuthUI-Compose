4697539be5796a868e01da07ee486afc64bf20f5
Muhammad Wafa <muhammadwafa105@gmail.com>
2023-08-26 14:50:47 +0700

    initial commit

---
+++ app/src/main/java/com/mikirinkode/authui/MainApp.kt
@@ -0,0 +1,11 @@
+package com.mikirinkode.authui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MainApp(
+    modifier: Modifier = Modifier
+) {
+
+}
